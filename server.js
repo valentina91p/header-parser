232d6c8e1c681050cd0450fa3bec8cd4aea27b8b
c-valentinamm <c.valentinamm@gmail.com>
2016-05-08 18:13:00 -0400

    fixed error on line 11

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var app = require("express")();
 
 app.get("/", function(req, res){
 	var info = {};
-	info.ipaddress = req.ip;
+	info.ipaddress = req.connection.remoteAddress || req.headers["X-Forwarded-For"] || req.headers["x-forwarded-for"];
 	info.language = req.acceptsLanguages()[0];
-	info.software = req.headers['user-agent'];
+	info.software = req.headers['user-agent'].split("(")[1].split(")")[0];
 	res.json(info);
 });
 app.listen(process.env.PORT || 80, function(){
-	console.log("Running on port ",(process.env.port || 80));
+	console.log("Running on port ",(process.env.PORT || 80));
 });
