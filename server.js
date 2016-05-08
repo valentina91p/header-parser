9dd3e6e8fa83d10a6ff7b2287d7d338c4f3cc062
c-valentinamm <c.valentinamm@gmail.com>
2016-05-08 14:44:37 -0400

    added code to return remote client info

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require("express")();
 
 express.get("/", function(req, res){
 	var info = {};
+	info.ipaddress = req.ip;
+	info.language = req.acceptsLanguages()[0];
+	info.software = req.headers['user-agent'];
 	res.json(info);
 });
 express.listen(process.env.port || 80, function(){
